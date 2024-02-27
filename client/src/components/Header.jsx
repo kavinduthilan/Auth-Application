ac47b018d18e7818771895e701aa6531ba2d7c60
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-02-27 18:18:58 +0530

    modified header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,20 +3,22 @@ import {Link} from 'react-router-dom'
 function Header() {
   return (
     <div className="bg-tertiary">
-        <div className="flex justify-between items-center">
-            <h1>Auth</h1>
-            <ul className="flex gap-4">
-              <Link to='/'>
-                <li>Home</li>
-              </Link>
-              <Link to='/about'>
-                <li>About</li>
-              </Link>        
-              <Link to='/sign-in'>
-                <li>Sign In</li>
-              </Link>        
-                       
-            </ul>
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link>
+          <h1 className='font-bold'>Auth</h1>
+        </Link>
+            
+        <ul className="flex gap-6">
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
+          <Link to='/about'>
+            <li>About</li>
+          </Link>        
+          <Link to='/sign-in'>
+            <li>Sign In</li>
+          </Link>    
+        </ul>
       </div>
       
     </div>
