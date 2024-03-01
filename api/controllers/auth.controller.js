01f0e99fe4ef9d0b8896ad268ff547c8f4b7225f
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-03-02 01:19:23 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -13,3 +13,31 @@ export const signup = async (req, res,next) => {
         next(error);
     }
 }
+
+
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found!'));
+        }
+        const validPassword = await bcrypt.compare(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'Invalid credentials!'));
+
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...res } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000); 
+        res
+            .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+            .status(200)
+            .json(validUser);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+
+}
